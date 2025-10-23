93a1dce9012c2448a181ffbcf46525957d2562f2
everton <evertonlima58811@gmail.com>
2025-10-23 16:41:15 -0300

    fix(links): ajusta caminhos absolutos no menu para evitar erros 404 entre páginas

--- php/partials/header.php
+++ php/partials/header.php
@@ -3,19 +3,19 @@
 <head>
   <meta charset="utf-8">
   <title>Café Moderno</title>
-  <link rel="stylesheet" href="../../css/style.css">
+  <link rel="stylesheet" href="/Projetos/projeto-caf-moderno/css/style.css">
 </head>
 <body>
-  <header>
+  <header class="site-header">
     <h1>☕ Café Moderno</h1>
-    <nav>
-      <a href="../../web/index.php">Início</a>
-      <a href="../cliente/index.php">Clientes</a>
-      <a href="../mesa/index.php">Mesas</a>
-      <a href="../cardapioitem/index.php">Cardápio</a>
-      <a href="../reserva/index.php">Reservas</a>
-      <a href="../pedido/index.php">Pedidos</a>
-      <a href="../pedido_item/index.php">Itens do Pedido</a>
+    <nav class="menu">
+      <a href="/Projetos/projeto-caf-moderno/web/index.php">Início</a>
+      <a href="/Projetos/projeto-caf-moderno/php/cliente/index.php">Clientes</a>
+      <a href="/Projetos/projeto-caf-moderno/php/mesa/index.php">Mesas</a>
+      <a href="/Projetos/projeto-caf-moderno/php/cardapioitem/index.php">Cardápio</a>
+      <a href="/Projetos/projeto-caf-moderno/php/reserva/index.php">Reservas</a>
+      <a href="/Projetos/projeto-caf-moderno/php/pedido/index.php">Pedidos</a>
+      <a href="/Projetos/projeto-caf-moderno/php/pedido_item/index.php">Itens do Pedido</a>
     </nav>
   </header>
   <hr>
